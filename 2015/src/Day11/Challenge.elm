f6edf08b4f338d0353f00f5857f54942ffdae56b
Robert Curry <robert.curry@lexer.io>
2021-09-09 09:46:01 +1000

    day 11 setup

---
+++ 2015/src/Day11/Challenge.elm
@@ -0,0 +1,12 @@
+module Day11.Challenge exposing (..)
+
+
+
+part1 : String -> String
+part1 input =
+  input
+
+
+part2 : String -> String
+part2 input =
+  input
---
+++ 2015/src/Day11/Input.elm
@@ -0,0 +1,9 @@
+module Day11.Input exposing (..)
+
+test : String
+test =
+  "hijklmmn"
+
+input : String
+input =
+  "hepxcrrq"
