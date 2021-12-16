4bd66156babfb19715047b3114ea19beb8e34376
Robert Curry <robert.curry@lexer.io>
2021-12-16 17:23:26 +1100

    day 15 updated to use dijkstra

--- 2021/day15/src/main.rs
+++ 2021/day15/src/main.rs
@@ -1,11 +1,15 @@
-use std::collections::HashSet;
+use std::collections::BinaryHeap;
+use std::time::Instant;
+use core::cmp::Ordering;
 
 fn main() {
     const INPUT: &str = include_str!("input.txt");
     //println!("input: {:?}", INPUT);
     println!("--- Day 15: Chiton ---");
-    println!("problem1: {}", solve_problem1(INPUT));
-    println!("problem2: {}", solve_problem2(INPUT));
+    let start = Instant::now();
+    println!("problem1: {}\nduration: {:?}", solve_problem1(INPUT), start.elapsed());
+    let start = Instant::now();
+    println!("problem2: {}\nduration: {:?}", solve_problem2(INPUT), start.elapsed());
 }
 
 fn parse_input(input: &str) -> Vec<Vec<usize>> {
@@ -23,91 +27,85 @@ fn parse_input(input: &str) -> Vec<Vec<usize>> {
         .collect::<Vec<_>>()
 }
 
-#[derive(Debug, Copy, Clone)]
-struct Node {
-    g_cost: usize,
-    h_cost: usize,
-    f_cost: usize,
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State {
     cost: usize,
-    closed: bool
+    position: usize,
 }
 
-fn find_path_cost(start: (usize, usize), target: (usize, usize), map: &Vec<Vec<usize>>) -> usize {
-    let mut a_star = vec![];
-    let (tx, ty) = target;
-
-    // create node map
-    for (y, row) in map.iter().enumerate() {
-        let mut nodes = vec![];
-        for (x, &n) in row.iter().enumerate() {
-            let g_cost = 0;
-            let h_cost = tx - x + ty - y;
-            let f_cost = g_cost + h_cost;
-            let cost = n;
-            nodes.push(Node{ g_cost, h_cost, f_cost, cost, closed: false })
-        }
-        a_star.push(nodes);
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+            .then_with(|| self.position.cmp(&other.position))
     }
+}
 
-    // set up
-    let mut open_set = HashSet::new();
-    open_set.insert(start);
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
-    // pathfinding
-    while open_set.len() > 0 {
-        // find next node
-        let (mut x, mut y) = open_set.iter().next().unwrap();
-        for &(i, j) in open_set.iter() {
-            let c = &a_star[y][x];
-            let n = &a_star[j][i];
-            if n.f_cost < c.f_cost {
-                x = i;
-                y = j;
+struct Edge {
+    node: usize,
+    cost: usize,
+}
+
+// Dijkstra's shortest path algorithm.
+fn shortest_path(adj_list: &Vec<Vec<Edge>>, start: usize, goal: usize) -> Option<usize> {
+    let mut dist: Vec<_> = (0..adj_list.len()).map(|_| usize::MAX).collect();
+    let mut heap = BinaryHeap::new();
+
+    dist[start] = 0;
+    heap.push(State { cost: 0, position: start });
+
+    while let Some(State { cost, position }) = heap.pop() {
+        if position == goal { return Some(cost); }
+        if cost > dist[position] { continue; }
+
+        for edge in &adj_list[position] {
+            let next = State { cost: cost + edge.cost, position: edge.node };
+            if next.cost < dist[next.position] {
+                heap.push(next);
+                dist[next.position] = next.cost;
             }
         }
+    }
 
-        // close node
-        a_star[y][x].closed = true;
-        open_set.remove(&(x,y));
+    None
+}
 
-        // check if target
-        if (x, y) == target {
-            return a_star[y][x].g_cost;
-        }
+fn create_graph(map: &Vec<Vec<usize>>) -> Vec<Vec<Edge>> {
+    let mut nodes = vec![];
+    let w = map[0].len();
+    let h = map.len();
 
-        // update and add neighbors to open set
-        let w = a_star[0].len() -1;
-        let h = a_star.len() -1;
-        let c = a_star[y][x].clone();
-        for &(dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
-            if (dx == -1 && x == 0) || (dx == 1 && x == w) { continue; }
-            if (dy == -1 && y == 0) || (dy == 1 && y == h) { continue; }
-
-            let i = (dx + x as isize) as usize;
-            let j = (dy + y as isize) as usize;
-
-            let n = &mut a_star[j][i];
-            if n.closed { continue; }
-
-            if open_set.contains(&(i, j)){
-                let g_cost = c.g_cost + n.cost;
-                if g_cost < n.g_cost {
-                    n.g_cost = g_cost;
-                }
-            } else {
-                open_set.insert((i,j));
-                n.g_cost = c.g_cost + n.cost;
+    for y in 0..h {
+        for x in 0..w {
+            let mut node = vec![];
+            for &(dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
+                if (dx == -1 && x == 0) || (dx == 1 && x == w-1) { continue; }
+                if (dy == -1 && y == 0) || (dy == 1 && y == h-1) { continue; }
+                
+                let i = (dx + x as isize) as usize;
+                let j = (dy + y as isize) as usize;
+                
+                node.push(Edge{ node: i + j*w, cost: map[j][i] })
             }
-            n.f_cost = n.g_cost + n.h_cost;
+            nodes.push(node);
         }
     }
-    unreachable!()
+
+    nodes
 }
 
 #[allow(unused)]
 fn solve_problem1(input: &str) -> usize {
     let map = parse_input(input);
-    find_path_cost((0, 0), (map[0].len()-1, map.len()-1), &map)
+    let graph = create_graph(&map);
+    let tx = map[0].len() - 1;
+    let ty = map.len() - 1;
+    shortest_path(&graph, 0, tx + ty*(tx+1)).unwrap()
 }
 
 #[allow(unused)]
@@ -138,7 +136,10 @@ fn solve_problem2(input: &str) -> usize {
     }
     map.extend(new_rows);
 
-    find_path_cost((0, 0), (map[0].len()-1, map.len()-1), &map)
+    let graph = create_graph(&map);
+    let tx = map[0].len() - 1;
+    let ty = map.len() - 1;
+    shortest_path(&graph, 0, tx + ty*(tx+1)).unwrap()
 }
 
 #[cfg(test)]
--- 2021/day16/src/main.rs
+++ 2021/day16/src/main.rs
@@ -1,28 +1,29 @@
 fn main() {
     const INPUT: &str = include_str!("input.txt");
     //println!("input: {:?}", INPUT);
+    println!("--- Day 16: Packet Decoder ---");
     println!("problem1: {}", solve_problem1(INPUT));
     println!("problem2: {}", solve_problem2(INPUT));
 }
 
 #[allow(unused)]
-fn solve_problem1(input: &str) -> i32 {
+fn solve_problem1(input: &str) -> usize {
     0
 }
 
 #[allow(unused)]
-fn solve_problem2(input: &str) -> i32 {
+fn solve_problem2(input: &str) -> usize {
     0
 }
 
 #[cfg(test)]
 mod test {
     use crate::{solve_problem1, solve_problem2};
-    const TEST_INPUT: &str = "";
+    const TEST_INPUT: &str = "A0016C880162017C3686B18A3D4780";
 
     #[test]
     fn problem1() {
-        let expected = 0;
+        let expected = 31;
         let actual = solve_problem1(TEST_INPUT);
         assert_eq!(expected, actual);
     }
