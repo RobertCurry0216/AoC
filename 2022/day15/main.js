416136794b29cd01aceb86f01844f453a9dab258
robertcurry0216 <robertcurry0216@gmail.com>
2022-12-15 21:50:47 +1100

    day 15 done

--- 2022/day15/main.js
+++ 2022/day15/main.js
@@ -1,23 +1,26 @@
 const fs = require("fs");
 
-re = /Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)/;
+re =
+  /Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)/;
 
 const buildData = (filePath) => {
-  const lines = fs.readFileSync(filePath, 'utf8').split(/\n/);
-  const data = []
+  const lines = fs.readFileSync(filePath, "utf8").split(/\n/);
+  const data = [];
 
   for (const line of lines) {
     const match = line.match(re);
     data.push({
-      sx: +match[1], sy: +match[2],
-      bx: +match[3], by: +match[4]
-    })
+      sx: +match[1],
+      sy: +match[2],
+      bx: +match[3],
+      by: +match[4],
+    });
   }
 
   return data;
-}
+};
 
-const manhattan = (x, y, a, b) => Math.abs(x - a) + Math.abs(y - b) 
+const manhattan = (x, y, a, b) => Math.abs(x - a) + Math.abs(y - b);
 
 const part1 = (filePath, target) => {
   const data = buildData(filePath);
@@ -27,11 +30,11 @@ const part1 = (filePath, target) => {
     const dist = manhattan(v.sx, v.sy, v.bx, v.by);
     if (v.sy + dist >= target && v.sy - dist <= target) {
       const diff = dist - Math.abs(target - v.sy);
-      targetSlices.push( [ v.sx - diff, v.sx + diff ] )
+      targetSlices.push([v.sx - diff, v.sx + diff]);
     }
   }
 
-  targetSlices.sort((a,b) => a[0] - b[0]);
+  targetSlices.sort((a, b) => a[0] - b[0]);
 
   let slice = targetSlices[0];
   let total = 0;
@@ -50,18 +53,43 @@ const part1 = (filePath, target) => {
   total += slice[1] - slice[0];
 
   return total;
-}
+};
 
+const part2 = (filePath, size) => {
+  const data = buildData(filePath);
 
-const part2 = (filePath) => {
-  const data = fs.readFileSync(filePath, 'utf8');
-  return 0;
-}
+  for (let y = 0; y < size; y++) {
+    const ySlices = data
+      .map((v) => {
+        const dist = manhattan(v.sx, v.sy, v.bx, v.by);
+        if (v.sy + dist >= y && v.sy - dist <= y) {
+          const diff = dist - Math.abs(y - v.sy);
+          return [v.sx - diff, v.sx + diff];
+        }
+      })
+      .filter((v) => v)
+      .map(([a, b]) => [Math.max(a, 0), Math.min(b, size)]);
 
+    ySlices.sort((a, b) => a[0] - b[0]);
+
+    let slice = ySlices[0];
+
+    for (let i = 1; i < ySlices.length; i++) {
+      const next = ySlices[i];
+      if (slice[1] >= next[0]) {
+        // overlap
+        slice[1] = Math.max(slice[1], next[1]);
+      } else {
+        // no overlap
+        return (slice[1] + 1) * 4000000 + y;
+      }
+    }
+  }
+};
 
 const main = () => {
-  console.log(`part 1: ${part1("./input.txt", 2000000)}`)
-  console.log(`part 2: ${part2("./input.txt")}`)
-}
+  console.log(`part 1: ${part1("./input.txt", 2000000)}`);
+  console.log(`part 2: ${part2("./input.txt", 4000000)}`);
+};
 
-main()
+main();
