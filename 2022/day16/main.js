86580aa1752491573db9583591b15e23465dbd60
Robert Curry <robert.curry@lexer.io>
2024-01-17 09:17:41 +1100

    stuff

--- 2022/day16/main.js
+++ 2022/day16/main.js
@@ -1,8 +1,54 @@
 const fs = require("fs");
 
+const createMap = (filePath) => {
+  const re = /Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z, ]+)/;
+  return fs.readFileSync(filePath, 'utf8')
+  .split("\n")
+  .reduce((acc, line)=>{
+    const m = line.match(re);
+    acc[m[1]] = {
+      rate: +m[2],
+      tunnels: m[3].split(", ")
+    }
+    return acc;
+  }, {});
+};
+
+const mostPresureWrapper = () => {
+  const cache = {};
+
+  const mostPressure = (map, addr, time) => {
+    if (time <= 0) return 0;
+    if (typeof (cache[addr] || [])[time] == "number") return cache[addr][time];
+
+    const location = map[addr];
+    const curValve = location.rate * (time-1);
+    let best = 0;
+    for (const tunnel of location.tunnels) {
+      best = Math.max(best, mostPressure(map, tunnel, time-1));
+      best = Math.max(best, curValve + mostPressure(map, tunnel, time-2));
+    }
+
+    if (!cache[addr]) cache[addr] = [];
+    cache[addr][time] = best;
+
+    return best;
+  };
+
+  return mostPressure;
+};
+
 const part1 = (filePath) => {
-  const data = fs.readFileSync(filePath, 'utf8');
-  return 0;
+  const map = createMap(filePath);
+  const loctions = Object.keys(map);
+  const mostPressure = mostPresureWrapper();
+  const steps = 30;
+  for (let i=1; i<steps; i++) {
+    loctions.forEach(
+      (l) => mostPressure(map, l, i)
+    )
+  }
+  return mostPressure(map, "AA", steps);
 }
 
 
@@ -13,8 +59,8 @@ const part2 = (filePath) => {
 
 
 const main = () => {
-  console.log(`part 1: ${part1("./input.txt")}`)
-  console.log(`part 2: ${part2("./input.txt")}`)
+  console.log(`part 1: ${part1("./sample.txt")}`)
+  console.log(`part 2: ${part2("./sample.txt")}`)
 }
 
 main()
